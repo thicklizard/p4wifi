40a8ed32c7829b5a98f6f36eae3d24ce379f0032
Thicklizard <thicklizard@gmail.com>
2013-11-14 19:40:05 -0600

    patch to aide in hotplugging

--- kernel/sched.c
+++ kernel/sched.c
@@ -73,6 +73,7 @@
 #include <linux/slab.h>
 #include <linux/cpuacct.h>
 #include <linux/init_task.h>
+#include "smpboot.h"
 
 #include <asm/tlb.h>
 #include <asm/irq_regs.h>
@@ -8581,6 +8582,7 @@ void __init sched_init(void)
 	/* May be allocated at isolcpus cmdline parse time */
 	if (cpu_isolated_map == NULL)
 		zalloc_cpumask_var(&cpu_isolated_map, GFP_NOWAIT);
+	idle_thread_set_boot_cpu(); 
 #endif /* SMP */
 
 	scheduler_running = 1;
